09f8df5e0411576003facbe58a3b17d5e2efaba9
Kieu Minh Hieu <hieukieuminh@gmail.com>
2021-05-07 21:05:18 +0700

    add run code

--- webapp/api/models.py
+++ webapp/api/models.py
@@ -1,6 +1,8 @@
 import pymongo
 from bson.objectid import ObjectId
 import logging
+from .runcode import RunCppCode
+
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["scoss"]
@@ -27,20 +29,27 @@ def get_code() -> dict:
 
 def save_code(id: str, label: str) -> dict:
     check_id = True
-    code = None
+    code_doc = None
     try:
-        code = mycol.find_one({"_id": ObjectId(id)})
+        code_doc = mycol.find_one({"_id": ObjectId(id)})
     except:
         logging.exception('Not found doc by id ' + id)
         check_id = False
-    if (code == None and check_id == False):
+    if (code_doc == None and check_id == False):
         return {
             'errCode': 400,
             'errMess': 'Not found doc by id ' + id
         }
     else:
-        count = int(code['count']) + 1
-        mycol.update_one({'_id': ObjectId(id)}, {"$set": {'label' + str(count): label, 'count': count}}, upsert=True)
+        count = int(code_doc['count']) + 1
+        run = RunCppCode(str(label), code_doc)
+        rescompil, resrun, save_label = run.run_cpp_code()
+
+        if rescompil == "":
+            print("ok")
+            #mycol.update_one({'_id': ObjectId(id)}, {"$set": {'label' + str(count): label, 'count': count}}, upsert=True)
+
         return {'errCode': 200,
-                'errMess': 'upload success'
+                'errMess': 'upload success',
+                'save_label': save_label
             }       
---
+++ webapp/api/runcode.py
@@ -0,0 +1,69 @@
+import subprocess
+import sys
+import os
+
+class RunCppCode(object):
+
+    def __init__(self, code=None, code_doc=None):
+        self.code = code
+        self.code_doc = code_doc
+        self.compiler = "g++"
+        if not os.path.exists('running'):
+            os.mkdir('running')
+
+    def _compile_cpp_code(self, filename, prog="./running/a.out"):
+        cmd = [self.compiler, filename, "-Wall", "-o", prog]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = p.wait()
+        a, b = p.communicate()
+        self.stdout, self.stderr = a.decode("utf-8"), b.decode("utf-8")
+        return result
+
+    def _run_cpp_prog(self, cmd="./running/a.out"):        
+        data, temp = os.pipe()
+        os.write(temp, bytes(str(self.code_doc["input"]), "utf-8"))
+        os.close(temp)
+
+        # run check output
+        s = subprocess.check_output(cmd, stdin = data, shell = True)
+
+        return s.decode("utf-8")
+
+    def run_cpp_code(self, code=None):
+        save_label = False
+        id_code = str(self.code_doc["_id"])
+        filename = "./running/" + id_code + ".cpp"
+        fileout = "./running/" + id_code + ".out"
+
+        if not code:
+            code = self.code
+        result_run = "No run done"
+
+        # write code to file
+        with open(filename, "w") as f:
+            f.write(code)
+
+        #compiler code
+        res = self._compile_cpp_code(filename, fileout)
+        result_compilation = self.stdout + self.stderr
+
+        if res == 0:
+            result_run = self._run_cpp_prog(fileout)
+
+            result_run = result_run.replace(" ", "").replace("/n", "")
+            
+            result_run_true = str(self.code_doc["output"]).replace(" ", "").replace("/n", "")
+            print('------------------')
+            print(result_run)
+            print(result_run_true)
+
+            if result_run == result_run_true:
+                save_label = True
+
+        # delete file
+        if os.path.exists(filename):
+            os.remove(filename)
+        if os.path.exists(fileout):
+            os.remove(fileout)
+
+        return result_compilation, result_run, save_label
